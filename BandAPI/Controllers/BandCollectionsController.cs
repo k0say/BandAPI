ce7b97cbc8e208ba80bd48a885a4a62b2aee1d7c
Giovanni Vittore <giovanni.vittore@alten.it>
2020-12-21 15:35:52 +0100

    Custom Model Binder

--- BandAPI/Controllers/BandCollectionsController.cs
+++ BandAPI/Controllers/BandCollectionsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using BandAPI.Helpers;
 using BandAPI.Models;
 using BandAPI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,23 @@ namespace BandAPI.Controllers
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
+        [HttpGet("({ids})", Name = "GetBandCollection")]
+        public IActionResult GetBandCollection([FromRoute][ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids is null)
+            {
+                return BadRequest();
+            }
+
+            var bandEntities = _bandAlbumRepository.GetBands(ids);
+
+            if (ids.Count() != bandEntities.Count())
+                return NotFound();
+
+            var bandsToReturn = _mapper.Map<IEnumerable<BandDto>>(bandEntities);
+
+            return Ok(bandsToReturn);
+        }
 
         [HttpPost]
         public ActionResult<IEnumerable<BandDto>> CreateBandCollection([FromBody] IEnumerable<BandForCreatingDto> bandCollection)
@@ -35,7 +53,10 @@ namespace BandAPI.Controllers
 
             _bandAlbumRepository.Save();
 
-            return Ok();
+            var bandsCollectionToReturn = _mapper.Map<IEnumerable<BandDto>>(bandEntities);
+            var IdsString = string.Join(",", bandsCollectionToReturn.Select(a => a.Id));
+
+            return CreatedAtRoute("GetBandCollection", new { ids = IdsString }, bandsCollectionToReturn);
         }
     }
 }
